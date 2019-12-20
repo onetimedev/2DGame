4b44f9edb05dc76ca244e80ee37a8235e576bb33
Ben Simms <ben@bensimms.moe>
2019-12-20 20:23:02 +0000

    Add deserialization methods to components

--- src/test/java/scc210game/ECSTest.java
+++ src/test/java/scc210game/ECSTest.java
@@ -1,36 +1,101 @@
 package scc210game;
 
+import com.github.cliftonlabs.json_simple.JsonObject;
+import com.github.cliftonlabs.json_simple.Jsonable;
+import com.github.cliftonlabs.json_simple.Jsoner;
 import org.junit.Test;
 import scc210game.ecs.System;
 import scc210game.ecs.*;
 
 import javax.annotation.Nonnull;
+import java.math.BigDecimal;
 import java.time.Duration;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
+class Velocity extends Component {
+    static {
+        register(Velocity.class, s -> {
+            final JsonObject json = Jsoner.deserialize(s, new JsonObject());
+
+            BigDecimal dx = (BigDecimal) json.get("dx");
+            BigDecimal dy = (BigDecimal) json.get("dy");
+
+            return new Velocity(dx.intValue(), dy.intValue());
+        });
+    }
+
+    public final int dx;
+    public final int dy;
+
+    public Velocity(int dx, int dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    @Override
+    public String serialize() {
+        final Jsonable json = new JsonObject() {{
+            this.put("dx", Velocity.this.dx);
+            this.put("dy", Velocity.this.dy);
+        }};
+
+        return json.toJson();
+    }
+}
+
+class Position extends Component {
+    static {
+        register(Position.class, s -> {
+            final JsonObject json = Jsoner.deserialize(s, new JsonObject());
+
+            BigDecimal x = (BigDecimal) json.get("x");
+            BigDecimal y = (BigDecimal) json.get("y");
+
+            return new Position(x.intValue(), y.intValue());
+        });
+    }
+
+    public int x, y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+
+        Position position = (Position) o;
+
+        if (this.x != position.x) return false;
+        return this.y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = this.x;
+        result = 31 * result + this.y;
+        return result;
+    }
+
+    @Override
+    public String serialize() {
+        final Jsonable json = new JsonObject() {{
+            this.put("x", Position.this.x);
+            this.put("y", Position.this.y);
+        }};
+
+        return json.toJson();
+    }
+}
+
 public class ECSTest {
     @Test
     public void testECS() {
-        class Velocity implements Component {
-            public final int dx;
-            public final int dy;
-
-            public Velocity(int dx, int dy) {
-                this.dx = dx;
-                this.dy = dy;
-            }
-        }
-
-        class Position implements Component {
-            public int x, y;
-
-            public Position(int x, int y) {
-                this.x = x;
-                this.y = y;
-            }
-        }
         class System0 extends System {
             @Nonnull
             @Override
@@ -65,5 +130,9 @@ public class ECSTest {
         var entPos = ecs.fetchComponent(ent, Position.class);
 
         assertEquals(entPos.x, 1);
+
+        Position testPos = new Position(100, -100);
+
+        assertEquals(testPos, Component.deserialize(testPos.serialize(), Position.class));
     }
 }
