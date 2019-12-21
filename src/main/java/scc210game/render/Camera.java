3ba6f82d8b0414f4e841cc174dfaf10ca82f699b
marshaj7 <j.d.marshall4@lancaster.ac.uk>
2019-12-21 17:34:09 +0000

    Start of class structures

---
+++ src/main/java/scc210game/render/Camera.java
@@ -0,0 +1,24 @@
+package scc210game.render;
+
+
+/**
+ * Represents the view of a region based on its zoom and size
+ */
+public class Camera {
+
+	private int centre;
+	//Need to store all entities in view, with priority
+
+	public Camera(int zoom, int width, int height) {
+
+	}
+
+	public void addToView() {
+
+	}
+
+
+
+
+
+}
---
+++ src/main/java/scc210game/render/Render.java
@@ -0,0 +1,23 @@
+package scc210game.render;
+
+import org.jsfml.graphics.RenderStates;
+import org.jsfml.graphics.RenderTarget;
+
+
+public class Render {
+
+	private RenderStates renderState;
+
+
+	public Render() {
+
+	}
+
+
+	public void update(RenderTarget rt) {
+
+	}
+
+
+
+}
