39eee5dee49b591846ba9987d7a03af9320d9529
emsleym <emsleym@lancaster.ac.uk>
2019-12-22 15:34:07 +0000

    creating methods

---
+++ src/main/java/scc210game/movement/movement.java
@@ -0,0 +1,14 @@
+package scc210game.movement;
+
+import org.jsfml.window.Keyboard;
+
+public class movement {
+    private char key;
+
+    //checks if a certain key is pressed
+    public boolean isKeyPressed(Keyboard.Key key) {
+
+        return true;
+    }
+
+}
