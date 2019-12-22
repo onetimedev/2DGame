87f1b3c12aaab41015203d77110a4d685dbb3f06
emsleym <emsleym@lancaster.ac.uk>
2019-12-22 17:38:32 +0000

    methods to check if keys w, a, s, d have been pressed

--- src/main/java/scc210game/movement/movement.java
+++ src/main/java/scc210game/movement/movement.java
@@ -3,12 +3,28 @@ package scc210game.movement;
 import org.jsfml.window.Keyboard;
 
 public class movement {
-    private char key;
+    char key;
 
-    //checks if a certain key is pressed
-    public boolean isKeyPressed(Keyboard.Key key) {
-//testing
-        return true;
-    }
-
+    //if key w, a, s, d is pressed return true, if not return false
+   public boolean keyPressed(Keyboard.Key W, Keyboard.Key A, Keyboard.Key S, Keyboard.Key D) {
+       if(Keyboard.isKeyPressed(W)) {
+           key = 'W';
+           return true;
+       }
+       else if(Keyboard.isKeyPressed(A)) {
+           key = 'A';
+           return true;
+       }
+       else if(Keyboard.isKeyPressed(S)) {
+           key = 'S';
+           return true;
+       }
+       else if(Keyboard.isKeyPressed(D)) {
+           key = 'D';
+           return true;
+       }
+       else {
+           return false;
+       }
+   }
 }
