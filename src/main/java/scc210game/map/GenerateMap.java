4fc9de4d938d09307fc83e602b1f7c7a147ca8e7
marshaj7 <43471906+Jvmes@users.noreply.github.com>
2020-01-07 15:44:27 +0000

    Tile constructor done, Skeletons for Map & GenerateMap

---
+++ src/main/java/scc210game/map/GenerateMap.java
@@ -0,0 +1,30 @@
+package scc210game.map;
+
+import org.jsfml.system.Vector2i;
+
+public class GenerateMap {
+
+	private Vector2i vector;
+
+
+	// to create a map create a map component and add it to this
+	// as single entity
+	//
+	// map tiles
+
+
+	public Vector2i generateVector() {
+
+
+		// load object containing position and sprite
+		// go though each of these values and add to tile
+
+		return vector;
+	}
+
+
+
+
+
+
+}
---
+++ src/main/java/scc210game/map/Map.java
@@ -0,0 +1,34 @@
+package scc210game.map;
+
+import scc210game.ecs.Component;
+
+/**
+ * Create map as part of the ECS
+ */
+public class Map extends Component {
+
+	@Override
+	public String serialize() {
+		return null;
+	}
+
+
+
+
+
+
+
+
+
+	/**
+	 * Method to create chests on tiles pseudo-random
+	 * Creating an entity with position component, renderable component and inventory component
+	 * giving position to entity based on the available spawnable tiles
+	 */
+	private void placeChests() {
+
+	}
+
+
+
+}
