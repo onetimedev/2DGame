c7da2791486e52cd82c19045ad11272e37ef063f
emsleym <m.emsley@lancaster.ac.uk>
2020-01-15 23:30:04 +0000

    added constructor and getter and setter methods for postion of player, and the key pressed

--- src/main/java/scc210game/movement/Movement.java
+++ src/main/java/scc210game/movement/Movement.java
@@ -1,13 +1,67 @@
 package scc210game.movement;
 
 import org.jsfml.window.Keyboard;
+import scc210game.ecs.Component;
 
-public class Movement {
-    char key;
-    int direc = 0;
+public class Movement extends Component {
+    private int posX;
+    private int posY;
+    private char key;
 
-    //if key w, a, s, d is pressed return true, if not return false
-   public boolean keyPressed(Keyboard.Key W, Keyboard.Key A, Keyboard.Key S, Keyboard.Key D) {
+    Movement(int posX, int posY) {
+        this.posY = posY;
+        this.posY = posY;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+    public int getPosX() {
+        return this.posX;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+    public int getPosY() {
+        return this.posY;
+    }
+
+    public void setKey(char key) {
+        this.key = key;
+    }
+    public char getKey() {
+        return this.key;
+    }
+
+
+
+
+   //Sets the direction the player is moving
+   /*public int direction(char key) {
+       if(key == 'W') {
+           direc = 0; //set the direction angle forward
+       }
+       else if(key == 'A') {
+           direc = 270; //set the direction angle left
+       }
+       else if(key == 'S') {
+           direc = 180; //set the direction angle backward
+       }
+       else if(key == 'D') {
+           direc = 90; //set the direction angle right
+       }
+       return direc;
+   }*/
+
+    @Override
+    public String serialize() {
+        return null;
+    }
+}
+
+//if key w, a, s, d is pressed return true, if not return false
+   /*public boolean keyPressed(Keyboard.Key W, Keyboard.Key A, Keyboard.Key S, Keyboard.Key D) {
        if(Keyboard.isKeyPressed(W)) {
            key = 'W';
            return true;
@@ -27,22 +81,4 @@ public class Movement {
        else {
            return false;
        }
-   }
-
-   //Sets the direction the player is moving
-   public int direction(char key) {
-       if(key == 'W') {
-           direc = 0; //set the direction angle forward
-       }
-       else if(key == 'A') {
-           direc = 270; //set the direction angle left
-       }
-       else if(key == 'S') {
-           direc = 180; //set the direction angle backward
-       }
-       else if(key == 'D') {
-           direc = 90; //set the direction angle right
-       }
-       return direc;
-   }
-}
+   }*/
