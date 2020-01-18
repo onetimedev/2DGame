7b96eab541f5a652e320cdddd0773e225fa52601
marshaj7 <j.d.marshall4@lancaster.ac.uk>
2020-01-18 13:51:27 +0000

    MapSpawner creates tiles around player coords based on the size of the window

--- src/main/java/scc210game/spawners/MapSpawner.java
+++ src/main/java/scc210game/spawners/MapSpawner.java
@@ -1,6 +1,10 @@
 package scc210game.spawners;
 
+import org.jsfml.graphics.RectangleShape;
 import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.Sprite;
+import org.jsfml.graphics.View;
+import org.jsfml.system.Vector2f;
 import org.jsfml.system.Vector2i;
 import scc210game.ecs.Entity;
 import scc210game.ecs.Spawner;
@@ -15,27 +19,45 @@ public class MapSpawner implements Spawner {
 		return builder
 				.with(new Map())
 				.with(new Renderable((Entity entity, RenderWindow window, World world) -> {
-					// TODO: Go through all tiles, get texture, translate to location it should be, then w.draw(texture)
-					// TODO: for each tile
-
 					Map m = world.fetchComponent(entity, Map.class);
-					Vector2i testPlayerCoords = new Vector2i(0,0);
+
+					Vector2i playerCoords = new Vector2i(40,32);  //TODO: Player entity get current position coords
+
+					// Number of tiles that can fit in windows X and Y
+					int tilesX = (int) Math.ceil(window.getSize().x / 64.0);
+					int tilesY = (int) Math.ceil(window.getSize().y / 64.0);
+
+					int positionX = 0;  // Sprites X Position in window
+					int positionY = 0;  // Sprites Y Position in window
+					int tilesLeft = (int) Math.floor(tilesX / 2.0);  // Number of tiles left of player X coord
+					int startX = playerCoords.x - tilesLeft;  // First tile X coord to be rendered from
+					int tilesTop = (int) Math.floor(tilesY / 2.0);  // Number of tiles above player Y coord
+					int startY = playerCoords.y - tilesTop;  // First tile Y coord to be rendered from
+
+					System.out.println("TilesX: " + tilesX);
+					System.out.println("TilesY: " + tilesY);
+
+					// Goes through each X,Y coordinate around the player that can be rendered and
+					// renders the tile at this X,Y coordinate
+					for(int y=0; y<tilesY; y++) {
+						for(int x=0; x<tilesX; x++) {
+							Sprite tile = new Sprite(m.getTile(startX, startY).getTexture());
+							tile.setPosition(positionX, positionY);
+							System.out.println("Tile " + startX + "," + startY + " at Position " + positionX + "," + positionY);
+							window.draw(tile);
+							startX++;
+							positionX += 64;
+						}
+						startX = playerCoords.x - tilesLeft;
+						startY++;
+						positionX = 0;
+						positionY += 64;
+					}
 
 
-					// number of tiles that can fit in window
-					int tilesInWindow = (window.getSize().x / 64) * (window.getSize().y / 64);
-					// work out the top left most renderable tile from the player coords and then
-					// 
-
-
-					//Position player = world.fetchComponent(entity, Player.class);
-					// Based on coords look at tiles around this that would fit in the window size
-
-
-					// w.draw(tile);
+					// Tiles currently changing scale on window size change
+					// need them to remain the same size
 
 				}, 0));  // Everything in map will have depth 0
 	}
-
-
 }
