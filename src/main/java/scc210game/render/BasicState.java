cd467789a2e7a8217e36f2b05e5a9aec8c870b72
marshaj7 <j.d.marshall4@lancaster.ac.uk>
2020-01-19 21:08:43 +0000

    'Merge' render code from 'impl-map-system' branch

---
+++ src/main/java/scc210game/render/BasicState.java
@@ -0,0 +1,12 @@
+package scc210game.render;
+
+import scc210game.ecs.World;
+import scc210game.spawners.MapSpawner;
+import scc210game.state.State;
+
+public class BasicState implements State {
+	@Override
+	public void onStart(World world) {
+		world.entityBuilder().with(new MapSpawner()).build();
+	}
+}
---
+++ src/main/java/scc210game/render/EngineSetup.java
@@ -0,0 +1,132 @@
+package scc210game.render;
+
+import org.jsfml.graphics.*;
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.system.Vector2i;
+import org.jsfml.window.VideoMode;
+import org.jsfml.window.event.Event;
+import org.jsfml.window.event.KeyEvent;
+import org.jsfml.window.event.MouseButtonEvent;
+import org.jsfml.window.event.MouseEvent;
+import scc210game.ecs.ECS;
+import scc210game.state.event.StateEvent;
+
+import java.util.List;
+
+
+/**
+ * Singleton class to hold game loop, take entities to render, and change mainWindow views
+ */
+public class EngineSetup {
+
+	private static EngineSetup instance = null;
+	public RenderWindow mainWindow;
+	// Add all systems / groups of systems here into the List
+	private ECS ecs;
+
+	private EngineSetup() {
+		mainWindow = new RenderWindow();
+		ecs = new ECS(List.of(new RenderSystem(mainWindow)), new BasicState());
+		ecs.start();
+		createWindow(720, 480);
+	}
+
+	
+	/**
+	 * To create or get the instance of the singleton class
+	 * @return the singleton instance of Render
+	 */
+	public static EngineSetup getInstance() {
+		if(instance == null)
+			instance = new EngineSetup();
+		return instance;
+	}
+
+
+	/**
+	 * Create a window given a size
+	 * @param width of the window
+	 * @param height of the window
+	 */
+	public void createWindow(int width, int height) {
+		mainWindow.create(new VideoMode(width, height), "Explore");
+		mainLoop();
+	}
+
+
+	/**
+	 * Runs the game loop from the createWindow method
+	 *  Takes events from the MainWindow and translates them to
+	 *  proprietary StateEvent for the EventQueue
+	 */
+	private void mainLoop() {
+		while(mainWindow.isOpen()) {
+			tilesInWindow();
+			mainWindow.clear(Color.BLACK);
+			for(Event event: mainWindow.pollEvents()) {
+				StateEvent se = new StateEvent(){};
+				switch (event.type) {
+					case KEY_PRESSED: {
+						KeyEvent keyEvent = event.asKeyEvent();
+						se = new scc210game.state.event.KeyPressedEvent(keyEvent.key);
+						break;
+					}
+					case KEY_RELEASED: {
+						KeyEvent keyEvent = event.asKeyEvent();
+						se = new scc210game.state.event.KeyDepressedEvent(keyEvent.key);
+						break;
+					}
+					case MOUSE_BUTTON_PRESSED: {
+						MouseButtonEvent msBtnEvent = event.asMouseButtonEvent();
+						se = new scc210game.state.event.MouseButtonPressedEvent(msBtnEvent.position.x, msBtnEvent.position.y, msBtnEvent.button);
+						break;
+					}
+					case MOUSE_BUTTON_RELEASED: {
+						MouseButtonEvent msBtnEvent = event.asMouseButtonEvent();
+						se = new scc210game.state.event.MouseButtonDepressedEvent(msBtnEvent.position.x, msBtnEvent.position.y, msBtnEvent.button);
+						break;
+					}
+					case MOUSE_MOVED: {
+						MouseEvent msMoved = event.asMouseEvent();
+						se = new scc210game.state.event.MouseMovedEvent(msMoved.position.x, msMoved.position.y);
+						break;
+					}
+					case CLOSED: {
+						mainWindow.close();
+						break;
+					}
+				}
+			ecs.runWithUpdateOnce(se);
+			}
+		ecs.runOnce();
+		mainWindow.display();
+		}
+	}
+
+
+
+
+
+	public void tilesInWindow(/*Tile[][] tiles*/) {
+		double tileSize = 64;
+		Vector2i windowSize = mainWindow.getSize();
+		int tilesX = (int) Math.ceil(windowSize.x / tileSize);
+		int tilesY = (int) Math.ceil(windowSize.y / tileSize);
+		System.out.println("Window Width: " + windowSize.x + ". Tiles in width: " + tilesX);
+		System.out.println("Window height: " + windowSize.y + ". Tiles in height: " + tilesY);
+
+		// Need players coords. Then render tiles around player
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+}
