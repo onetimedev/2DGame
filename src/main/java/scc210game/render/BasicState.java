c4eb4a74fe1e4e4c6af6609dac81abe27278f93d
Ben Simms <ben@bensimms.moe>
2020-01-20 15:08:36 +0000

    Add demo text

--- src/main/java/scc210game/render/BasicState.java
+++ src/main/java/scc210game/render/BasicState.java
@@ -2,9 +2,11 @@ package scc210game.render;
 
 import scc210game.ecs.World;
 import scc210game.state.State;
+import scc210game.ui.spawners.DialogueSpawner;
 
 public class BasicState implements State {
 	@Override
 	public void onStart(World world) {
+		world.entityBuilder().with(new DialogueSpawner("Test")).build();
 	}
 }
