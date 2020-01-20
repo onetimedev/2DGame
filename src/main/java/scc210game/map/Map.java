fff6a348bd26682dffe29e4766322008b9014e94
Ben Simms <ben@bensimms.moe>
2020-01-20 17:14:08 +0000

    Patch Map stuff to work with ECS changes

--- src/main/java/scc210game/map/Map.java
+++ src/main/java/scc210game/map/Map.java
@@ -6,61 +6,50 @@ import scc210game.ecs.Component;
  * Create map as part of the ECS
  */
 public class Map extends Component {
-	private Tile[][] mapTiles;
-	private GenerateMap genMap;
+    private Tile[][] mapTiles;
+    private GenerateMap genMap;
 
 
-	@Override
-	public String serialize() {
-		return null;
-	}
+    @Override
+    public String serialize() {
+        return null;
+    }
 
 
-	public Map() {
-		genMap = new GenerateMap();
-		mapTiles = genMap.getAllTiles();
+    public Map() {
+        genMap = new GenerateMap();
+        mapTiles = genMap.getAllTiles();
+    }
 
-		}
+    public Tile[][] getMap() {
+        return mapTiles;
+    }
 
-	public Tile[][] getMap() {
-		return mapTiles;
-	}
+    public int getWidth() {
+        return genMap.getMapSize().x;
+    }
 
-	public int getWidth() {
-		return genMap.getMapSize().x;
-	}
+    public int getHeight() {
+        return genMap.getMapSize().y;
+    }
 
-	public int getHeight() {
-		return genMap.getMapSize().y;
-	}
+    public Tile getTile(int xPos, int yPos) {
+        return mapTiles[xPos][yPos];
+    }
 
-	public Tile getTile(int xPos, int yPos) {
-		return mapTiles[xPos][yPos];
-	}
+    public int getTileMaxX() {
+        return mapTiles.length;
+    }
 
-	public int getTileMaxX() {
-		return mapTiles.length;
-	}
+    public int getTileMaxY() {
+        return mapTiles[0].length;
+    }
 
-	public int getTileMaxY() {
-		return mapTiles[0].length;
-	}
-
-
-
-
-
-
-
-	/**
-	 * Method to create chests on tiles pseudo-random
-	 * Creating an entity with position component, renderable component and inventory component
-	 * giving position to entity based on the available spawnable tiles
-	 */
-	private void placeChests() {
-
-	}
-
-
-
+    /**
+     * Method to create chests on tiles pseudo-random
+     * Creating an entity with position component, renderable component and inventory component
+     * giving position to entity based on the available spawnable tiles
+     */
+    private void placeChests() {
+    }
 }
