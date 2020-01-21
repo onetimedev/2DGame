f047c706828a605a9fe423eb870528e889ecfe4c
emsleym <m.emsley@lancaster.ac.uk>
2020-01-21 11:49:34 +0000

    return builder with new player

--- src/main/java/scc210game/spawners/PlayerSpawner.java
+++ src/main/java/scc210game/spawners/PlayerSpawner.java
@@ -2,12 +2,15 @@ package scc210game.spawners;
 
 import scc210game.ecs.Spawner;
 import scc210game.ecs.World;
+import scc210game.map.Player;
 
 public class PlayerSpawner implements Spawner {
 
 
 	@Override
 	public World.EntityBuilder inject(World.EntityBuilder builder) {
-		return null;
+		return builder
+				.with(new Player());
+
 	}
 }
