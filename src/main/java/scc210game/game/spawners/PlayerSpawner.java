9dc471c22bf1bf60036ad7944faa89d8a2a40139
Ben Simms <ben@bensimms.moe>
2020-01-30 19:01:45 +0000

    Git borked modules

---
+++ src/main/java/scc210game/game/spawners/PlayerSpawner.java
@@ -0,0 +1,13 @@
+package scc210game.game.spawners;
+
+import scc210game.engine.ecs.Spawner;
+import scc210game.engine.ecs.World;
+
+public class PlayerSpawner implements Spawner {
+
+
+	@Override
+	public World.EntityBuilder inject(World.EntityBuilder builder) {
+		return null;
+	}
+}
