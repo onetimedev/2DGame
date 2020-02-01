875176238e93c630f1492bee7c4ccc78ef121d2e
Ben Simms <ben@bensimms.moe>
2020-02-01 19:39:43 +0000

    Implement adding delayed events

--- src/main/java/scc210game/events/EventQueue.java
+++ src/main/java/scc210game/events/EventQueue.java
@@ -2,10 +2,15 @@ package scc210game.events;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayDeque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
 
 /**
  * An event queue that allows events to be distributed to listeners.
@@ -22,10 +27,13 @@ public class EventQueue {
     private final HashMap<EventQueueReader, ArrayDeque<Event>> queues;
     @Nonnull
     private final HashMap<Class<? extends Event>, HashSet<EventQueueReader>> registered;
+    @Nonnull
+    private final DelayQueue<DelayedEvent> delayedEvents;
 
     public EventQueue() {
         this.queues = new HashMap<>();
         this.registered = new HashMap<>();
+        this.delayedEvents = new DelayQueue<>();
     }
 
     /**
@@ -96,6 +104,34 @@ public class EventQueue {
     }
 
     /**
+     * Broadcast an event to all listeners at the specified Instant.
+     *
+     * @param evt The event to broadcast
+     * @param ins When to broadcast the event
+     */
+    public static void broadcastAt(@Nonnull Event evt, @Nonnull Instant ins) {
+        getInstance().delayedEvents.add(new DelayedEvent(evt, ins));
+    }
+
+    /**
+     * Broadcast an event to all listeners after the specified Duration.
+     *
+     * @param evt The event to broadcast
+     * @param delay How long to wait before broadcasting the event
+     */
+    public static void broadcastIn(@Nonnull Event evt, @Nonnull Duration delay) {
+        broadcastAt(evt, Instant.now().plus(delay));
+    }
+
+    private static void updateDelayedEvents() {
+        var inst = getInstance();
+        DelayedEvent evt;
+        while ((evt = inst.delayedEvents.poll()) != null) {
+            broadcast(evt.e);
+        }
+    }
+
+    /**
      * Fetch an iterator of events for the listening `r`
      *
      * @param r The entity to get events for
@@ -108,11 +144,13 @@ public class EventQueue {
         return new Iterator<>() {
             @Override
             public boolean hasNext() {
+                updateDelayedEvents();
                 return !q.isEmpty();
             }
 
             @Override
             public Event next() {
+                updateDelayedEvents();
                 return q.poll();
             }
         };
@@ -124,5 +162,27 @@ public class EventQueue {
             EventQueue.instance = new EventQueue();
 
         return EventQueue.instance;
+    }
+
+    private static class DelayedEvent implements Delayed {
+        public final Event e;
+        public final Instant end;
+
+
+        private DelayedEvent(Event e, Instant end) {
+            this.e = e;
+            this.end = end;
+        }
+
+        @Override
+        public long getDelay(TimeUnit unit) {
+            return Instant.now().until(this.end, unit.toChronoUnit());
+        }
+
+        @Override
+        public int compareTo(@Nonnull Delayed o) {
+            var de = (DelayedEvent) o;
+            return this.end.compareTo(de.end);
+        }
     }
 }
