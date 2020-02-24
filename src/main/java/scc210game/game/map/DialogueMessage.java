516aa3b4ddb07aa68a5de572bbfd388c70a91824
marshaj7 <j.d.marshall4@lancaster.ac.uk>
2020-02-24 18:24:40 +0000

    +Dialogues for all entities

--- src/main/java/scc210game/game/map/DialogueMessage.java
+++ src/main/java/scc210game/game/map/DialogueMessage.java
@@ -59,19 +59,35 @@ public class DialogueMessage {
 
 
 		private void grassEnemyDialogue() {
+			ArrayList<String> possDialogs = new ArrayList<>();
+			possDialogs.add("The shrowded figure remained disturbingly silent.");
+			possDialogs.add("Yellow eyes were the only features that could be made out on the mysterious foe.");
 
+			message += possDialogs.get(randomMessage(possDialogs.size()));
 		}
 
 		private void waterEnemyDialogue() {
+			ArrayList<String> possDialogs = new ArrayList<>();
+			possDialogs.add("The creature resembled the water that surrounded it, luckily it could be seen on the sand!");
+			possDialogs.add("While water has been known to kill, this creature looks like it will make it as painful as possible.");
 
+			message += possDialogs.get(randomMessage(possDialogs.size()));
 		}
 
 		private void fireEnemyDialogue() {
+			ArrayList<String> possDialogs = new ArrayList<>();
+			possDialogs.add("Looking as if it has just crawled from the depths of one of the many lava pools, surely it must be as dangerous.");
+			possDialogs.add("The creature sounded as if it was on fire, unfortunately not like the gentle crackling of logs.");
 
+			message += possDialogs.get(randomMessage(possDialogs.size()));
 		}
 
 		private void iceEnemyDialogue() {
+			ArrayList<String> possDialogs = new ArrayList<>();
+			possDialogs.add("Clearly a foe as old as the ice that surrounded it.");
+			possDialogs.add("A hiss came from the creature, a warning to any overly brave travellers of what might be yet to come.");
 
+			message += possDialogs.get(randomMessage(possDialogs.size()));
 		}
 
 
@@ -101,6 +117,8 @@ public class DialogueMessage {
 		ArrayList<String> possDialogs = new ArrayList<>();
 		possDialogs.add("Most people lost everything when the monsters came, but me, I wasn’t good at anything, but now I get paid a kings fee to hunt monsters.");
 		possDialogs.add("Have you seen my friend Joseph, he went to hunt some food but never came back, I hope he’s alright.");
+		possDialogs.add("Its gotten colder since they arrived, now we've known it like this for generations.");
+		possDialogs.add("I don't dare go far, have you heard those creatures hissing?");
 
 		message += possDialogs.get(randomMessage(possDialogs.size()));
 	}
@@ -108,7 +126,9 @@ public class DialogueMessage {
 	private void fireNPCDialogue() {
 		ArrayList<String> possDialogs = new ArrayList<>();
 		possDialogs.add("I can’t believe our beloved king, sits in his high castle eating like a pig, while we are all starving and getting slaughtered by the monsters.");
-		"I’ve heard that these monsters are from hell and the devil has come up from the underworld to punish us all.");
+		possDialogs.add("I’ve heard that these monsters are from hell and the devil has come up from the underworld to punish us all.");
+		possDialogs.add("It's a dangerous place sure, still be better of without these monsters!");
+		possDialogs.add("They've made it hotter than ever, I don't know how much more I can take.");
 
 		message += possDialogs.get(randomMessage(possDialogs.size()));
 	}
@@ -117,13 +137,15 @@ public class DialogueMessage {
 		ArrayList<String> possDialogs = new ArrayList<>();
 		possDialogs.add("Before all these monsters plagued this land, I had everything, my own shop, house, family, but now I have nothing.");
 		possDialogs.add("This place used to be so beautiful, now look at it.");
+		possDialogs.add("I'd never go in the water, not now.");
+		possDialogs.add("I used to love swimming.");
 
 		message += possDialogs.get(randomMessage(possDialogs.size()));
 	}
 
 	private void grassNPCDialogue() {
 		ArrayList<String> possDialogs = new ArrayList<>();
-		possDialogs.add("I lost my wife and son to these hideous monsters");
+		possDialogs.add("I lost my wife and son to these hideous monsters.");
 		possDialogs.add("I really hope our kings saves us soon!");
 		possDialogs.add("I have a theory that someone is controlling all these monsters, there’s no way they can all be this coordinated without a leader.");
 
@@ -131,11 +153,16 @@ public class DialogueMessage {
 	}
 
 
-
-
 	private void chestDialogue() {
+		ArrayList<String> possDialogs = new ArrayList<>();
+		possDialogs.add("What could be inside?");
+		possDialogs.add("It doesn't appear to be locked, i'm sure no one will notice the contents missing.");
+		possDialogs.add("It might take some force to keep such a chest open.");
+		possDialogs.add("Such beautiful craftsmanship, hopefully whatever is inside shares this quality.");
 
-		}
+		message += possDialogs.get(randomMessage(possDialogs.size()));
+
+	}
 
 
 
@@ -181,12 +208,13 @@ public class DialogueMessage {
 
 
 	private void finalBossDialogue() {
+		message = "The largest elemental yet, uh oh.";
 
 	}
 
 
 	private int randomMessage(int numOfMsg) {
-		int rng = (int) (Math.random() * numOfMsg + 1);
+		int rng = (int) (Math.random() * numOfMsg);
 		return rng;
 	}
 
