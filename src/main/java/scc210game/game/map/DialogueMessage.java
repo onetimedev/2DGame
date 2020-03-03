552eff52cee95b5f9e2cdf08db4f7b1c70a3d353
marshaj7 <j.d.marshall4@lancaster.ac.uk>
2020-03-03 22:46:41 +0000

    +Intro dialogue on MainGameState start

--- src/main/java/scc210game/game/map/DialogueMessage.java
+++ src/main/java/scc210game/game/map/DialogueMessage.java
@@ -314,8 +314,10 @@ public class DialogueMessage {
 
 	public String getIntroDialogue() {
 		String s = "After many years you have returned home, to revisit the communities and areas where you once lived.\n" +
-				"However no longer present are those who you once knew! Now the inhabitants of your home are elemental creatures, intent on controlling the elements at any cost!\n" +
-				"Move using W A S D    |    Enter or Q for dialogs    |    SPACE to attack    |    ESC to pause    |    ";
+				"However no longer present are those who you once knew! " +
+				"\nNow the inhabitants of your home are elemental creatures, intent on controlling the elements at any cost!\n" +
+				"\nMove using W A S D    |    ENTER or Q for dialogs    |    SPACE to attack    |    ESC to exit inventory or pause    |    CLICK top left chest icon for inventory" +
+				"\n\n----- Press Q or ENTER to Continue -----";
 
 		return s;
 	}
--- src/main/java/scc210game/game/states/MainGameState.java
+++ src/main/java/scc210game/game/states/MainGameState.java
@@ -7,6 +7,8 @@ import scc210game.engine.events.LeaveCombatEvent;
 import scc210game.engine.state.event.StateEvent;
 import scc210game.engine.state.trans.TransPush;
 import scc210game.engine.state.trans.Transition;
+import scc210game.game.events.DialogueCreateEvent;
+import scc210game.game.map.DialogueMessage;
 import scc210game.game.map.Map;
 import scc210game.game.map.Player;
 import scc210game.game.map.Tile;
@@ -65,7 +67,12 @@ public class MainGameState extends BaseInGameState {
 				}
         world.entityBuilder().with(new FinalBossSpawner(20, id)).build();
 
-    }
+
+				world.eventQueue.broadcast(new DialogueCreateEvent(new DialogueMessage().getIntroDialogue(),
+						(e, w) -> System.out.println(""),
+						(e, w) -> System.out.println("")));
+	}
+
 
 	@Override
 	public Transition handleEvent(StateEvent evt, World world) {
