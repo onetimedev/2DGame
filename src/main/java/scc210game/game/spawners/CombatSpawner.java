2b29d2f39491faf9a97c4aeb564d6c6daa48086a
xavierhickman <xavierhickman1234@gmail.com>
2020-03-05 16:15:36 +0000

    changed y

--- src/main/java/scc210game/game/spawners/CombatSpawner.java
+++ src/main/java/scc210game/game/spawners/CombatSpawner.java
@@ -68,7 +68,7 @@ public class CombatSpawner implements Spawner {
         double height = (double) t.getSize().y / 1080;
         float fheight = (float) (height);
 
-        yPosition = fMaxHeight - fheight + 0.05f;
+        yPosition = fMaxHeight - 0.3f;
 
 
     }
