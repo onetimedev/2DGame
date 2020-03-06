d8cbbb67a0c3a0086949121eecbd964ca4fd9e64
Ben Simms <ben@bensimms.moe>
2020-03-06 02:02:29 +0000

    Support arm and x86 libXinitThreads

--- libXinitThreads.cpp
+++ libXinitThreads.cpp
@@ -1,9 +1,14 @@
-#include <Xlib.h>
+#include <dlfcn.h>
+
+extern "C" typedef int (*XInitThreads_t)(void);
 
 class a {
   public:
   a() {
-    XInitThreads();
+    void *lib = dlopen("libX11.so", RTLD_LAZY);
+    XInitThreads_t xInitThreads = (XInitThreads_t)dlsym(lib, "XInitThreads");
+    xInitThreads();
+    dlclose(lib);
   }
 };
 
