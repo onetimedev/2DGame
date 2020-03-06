59a99983feae14b16dfd820e4f6e7f77dc6f5405
Ben Simms <ben@bensimms.moe>
2020-03-06 13:12:45 +0000

    Cache resources

--- src/main/java/scc210game/engine/utils/ResourceLoader.java
+++ src/main/java/scc210game/engine/utils/ResourceLoader.java
@@ -1,12 +1,28 @@
 package scc210game.engine.utils;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class ResourceLoader {
+    private static final HashMap<String, byte[]> cache = new HashMap<>();
+
     private static final ClassLoader loader = ClassLoader.getSystemClassLoader();
 
     public static InputStream resolve(String s) {
-        return Objects.requireNonNull(loader.getResourceAsStream(s));
+        byte[] bytes;
+        if (cache.containsKey(s))
+            bytes = cache.get(s);
+        var stream = Objects.requireNonNull(loader.getResourceAsStream(s));
+        try {
+            bytes = stream.readAllBytes();
+            cache.put(s, bytes);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new ByteArrayInputStream(bytes);
     }
 }
